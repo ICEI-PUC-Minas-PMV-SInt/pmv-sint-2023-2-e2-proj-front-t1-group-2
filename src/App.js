3e65613576ddc264851e234d1c876a5470a2be95
murilorezende <1469339@sga.pucminas.br>
2023-10-16 16:12:54 -0300

    16/10

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React from 'react';
-import './App.css';
+import { NavLink, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FaHome, FaInfoCircle, FaSignInAlt, FaSearch, FaCopyright } from 'react-icons/fa';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
-import { FaCopyright } from "react-icons/fa";
 import BarraMenu from './pages/BarraMenu';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <BarraMenu />
-      <BrowserRouter>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <NavLink to="/" className="navbar-brand">Currículos</NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink to="/Home" className="nav-link" activeClassName="active"><FaHome /> Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/About" className="nav-link" activeClassName="active"><FaInfoCircle /> About</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/Login" className="nav-link" activeClassName="active"><FaSignInAlt /> Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Search" />
+                  <div className="input-group-append">
+                    <span className="input-group-text" id="basic-addon2"><FaSearch /></span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
-      </BrowserRouter>
-      <footer>
-        <FaCopyright />Grupo 2
-      </footer>
-    </div>
+
+        <footer className="footer mt-auto py-3 text-center">
+          <FaCopyright /> Grupo 2
+        </footer>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript and Popper.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Home(props) {
-    return (
-        <div className="container mt-5">
-            <h1 className="text-center mb-4"></h1>
-            <p>
-                É amplamente reconhecido que a internet tem encurtado distâncias e provocado uma série de transformações nas nossas práticas de consumo, abrangendo áreas como entretenimento, compras online, métodos de estudo e até mesmo a busca por oportunidades de trabalho. De acordo com dados extraídos da Pesquisa dos Profissionais Brasileiros de 2019, conduzida pela Catho e que contou com a participação de mais de 6,2 mil entrevistados, a maioria dos profissionais atualmente recorre à internet em busca de oportunidades de emprego, sendo que 78% deles direcionam suas pesquisas para sites especializados em vagas de trabalho. 
+const perfis = [
+    { nome: 'João da Silva', profissao: 'Psicólogo', foto: 'imagens/bruno.jpg' },
+    { nome: 'Maria Oliveira', profissao: 'Engenheira Civil', foto: 'caminho/para/imagem2.jpg' },
+    { nome: 'Carlos Santos', profissao: 'Web Developer', foto: 'caminho/para/imagem3.jpg' },
+    { nome: 'Ana Costa', profissao: 'Advogada', foto: 'caminho/para/imagem4.jpg' },
+    { nome: 'Pedro Almeida', profissao: 'Professor', foto: 'caminho/para/imagem5.jpg' },
+    { nome: 'Laura Mendes', profissao: 'Designer Gráfico', foto: 'caminho/para/imagem6.jpg' },
+    { nome: 'Rafael Lima', profissao: 'Médico', foto: 'caminho/para/imagem7.jpg' },
+    { nome: 'Fernanda Oliveira', profissao: 'Arquiteta', foto: 'caminho/para/imagem8.jpg' },
+    { nome: 'Lucas Pereira', profissao: 'Programador', foto: 'caminho/para/imagem9.jpg' },
+    { nome: 'Beatriz Souza', profissao: 'Nutricionista', foto: 'caminho/para/imagem10.jpg' },
+    { nome: 'Marcos Silva', profissao: 'Analista de Dados', foto: 'caminho/para/imagem11.jpg' },
+    { nome: 'Isabel Santos', profissao: 'Artista Plástico', foto: 'caminho/para/imagem12.jpg' },
+];
 
-Segundo informações veiculadas no portal Melhor RH, a pesquisa realizada pela Catho em 2020 estima que as pessoas que optam por procurar emprego de forma convencional, ou seja, indo pessoalmente a empresas e distribuindo currículos, incorrem em um custo mensal médio de cerca de R$ 300. Esse cálculo leva em consideração despesas relacionadas a transporte, alimentação, impressão de currículos e uso de internet móvel. Esses números destacam a relevância do ambiente online não apenas para a redução de custos, mas também para ampliar o alcance das oportunidades disponíveis, uma vez que a busca por emprego na internet é mais ágil e abrangente. 
+const Perfil = ({ nome, profissao, foto }) => (
+  <div className="perfil-container col-md-6 mb-4 mt-3 mx-4">
+    <img src={foto} alt={`Foto do Perfil ${nome}`} className="foto" />
+    <div className="info">
+      <h3>{nome}</h3>
+      <h4>{profissao}</h4>
+    </div>
+  </div>
+);
 
-Nesse contexto, o presente projeto foi concebido como uma resposta às dificuldades enfrentadas por aqueles que desejam criar currículos de fácil acesso e facilitar a divulgação de produtos e serviços, especialmente para trabalhadores autônomos atuando no cenário digital. O objetivo central deste projeto é simplificar a criação de currículos virtuais e portfólios, abrangendo diversas áreas, incluindo a venda de serviços. Além disso, busca-se facilitar os processos de divulgação e pagamento, para quem trabalha com venda de serviços e produtos, além de proporcionar agilidade para aqueles que estão em busca de novas oportunidades (“CERCA DE 80% DOS PROFISSIONAIS PROCURAM OPORTUNIDADES ONLINE”, 2020).
-                </p>
-            </div>
-        );
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4"> </h1>
+
+      {perfis.map((perfil, index) => (
+        <Perfil key={index} {...perfil} />
+      ))}
+
+      <p>
+        {/* Seu texto aqui */}
+      </p>
+    </div>
+  );
 }
 
 export default Home;
