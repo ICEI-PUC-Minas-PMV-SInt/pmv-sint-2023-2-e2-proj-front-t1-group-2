d95b0f3bbda4c5095e6d08bf895888d566ce4f01
Isabella Prado <bellinhaprrado@gmail.com>
2023-10-24 22:07:08 -0400

    Corrigir erros
    Corrigi erro no link: equeci minha senha.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Login() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+    <Container style={{marginTop: 80 + 'px'}}>
+      <Row>
+        <Col></Col>
+        <Col xs={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Digite seu email" />
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-  
-      <Button variant="primary" type="submit">
-        Logar
-      </Button>
-    <NavLink to="/Esqueci" className="nav-link" activeClassName="active">Esqueci minha senha</NavLink>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Digite sua senha" />
+            </Form.Group>
+            <Nav.Link href="/Esqueci" >Esqueci minha senha</Nav.Link>
+            <Button variant="primary" type="submit"  style={{marginTop: 10 + 'px'}}>
+              Logar
+            </Button>
+
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+
+    </Container>
+
   );
 }
 
