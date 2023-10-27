91bed3bbb4adee3538bf12637609784ea0a975b9
Isabella Prado <bellinhaprrado@gmail.com>
2023-10-26 21:34:48 -0400

    Atualiza a barra menu
    Separa o barra menu para um componente a parte.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { NavLink, BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { FaHome, FaInfoCircle, FaSignInAlt, FaSearch, FaCopyright } from 'react-icons/fa';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { FaCopyright } from 'react-icons/fa';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
@@ -7,40 +7,14 @@ import Esqueci from './pages/Esqueci';
 import Erro from './pages/Erro';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import BarraMenu from './pages/BarraMenu';
 
 function App() {
 
   return (
     <Router>
+      <BarraMenu />
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <NavLink to="/" className="navbar-brand">Currículos</NavLink>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink to="/Home" className="nav-link" activeClassName="active"><FaHome /> Home</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/About" className="nav-link" activeClassName="active"><FaInfoCircle /> About</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/Login" className="nav-link" activeClassName="active"><FaSignInAlt /> Login</NavLink>
-              </li>
-              <li className="nav-item">
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Search" />
-                  <div className="input-group-append">
-                    <span className="input-group-text" id="basic-addon2"><FaSearch /></span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
         <Routes>
          	<Route exact path='/' element={<Home />} />
          	<Route path="/Home" element={<Home />} />
--- src/pages/BarraMenu.js
+++ src/pages/BarraMenu.js
@@ -1,31 +1,62 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { FaHome, FaInfoCircle, FaSignInAlt, FaSearch } from 'react-icons/fa';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 function BarraMenu() {
+    function isLogado() {
+        return localStorage.getItem("user") != null;
+    }
+
     return (
-        <Navbar className="bg-body-tertiary justify-content-between">
-            <Nav.Link href="/Home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Form inline>
-                <Row>
-                    <Col xs="auto">
-                        <Form.Control
-                            type="text"
-                            placeholder=""
-                            className=" mr-sm-2"
-                        />
-                    </Col>
-                    <Col xs="auto">
-                        <Button type="submit">Pesquisar</Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Navbar>
+        <>
+            {['sm'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Currículos</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="/Home"><FaHome /> Home</Nav.Link>
+                                    <Nav.Link href="/about"><FaInfoCircle /> About</Nav.Link>
+                                    {isLogado()
+                                        ? <Nav.Link href="/login"><FaSignInAlt /> Logout</Nav.Link>
+                                        : <Nav.Link href="/login"><FaSignInAlt /> Login</Nav.Link>
+                                    }
+                                </Nav>
+                                <Form className="d-flex">
+                                    <InputGroup className="mb-3">
+                                        <Form.Control
+                                            placeholder="Pesquisar"
+                                            aria-label="Pesquisar"
+                                            aria-describedby="basic-addon2"
+                                        />
+                                        <Button variant="outline-secondary" id="button-addon2">
+                                            <FaSearch />
+                                        </Button>
+                                    </InputGroup>
+                                </Form>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
     );
 }
 
